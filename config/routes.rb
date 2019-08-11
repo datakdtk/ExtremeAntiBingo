e195ae04630e0fbc4fb0e93231feb2ea00844eb3
Okada Takuya <takuya.okada@link-u.co.jp>
2019-08-11 01:50:57 +0000

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index', to: 'index#index'
   get 'output', to: 'output#index'
   get 'admin', to: 'admin#index'
   post 'admin/cards', to: 'admin#post_card'
